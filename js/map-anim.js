3d1da240b655300f60d9e832ce3331d29e177124
César Greppin <cesar.greppin@ringier.ch>
2017-09-14 16:58:35 +0200

    trad infog

--- js/map-anim.js
+++ js/map-anim.js
@@ -1,204 +1,115 @@
 jQuery(document).ready(function($) 
-{
+{	
+	var $erosionBlue = $('.erosion-blue');
+	var $erosionRock1 = $('.erosion-rock1');
+	var $erosionRock2 = $('.erosion-rock2');
+	var $erosionRock3 = $('.erosion-rock3');
+	var $erosionRock4 = $('.erosion-rock4');
+	var $erosionRock5 = $('.erosion-rock5');
 
-	var $segmentAnim1 = $('.fixed-map-wrapper svg #path-phoenix-nogales path');
-	var $dotAnim1 = $('.fixed-map-wrapper svg #nogales-dot #step1-dot_2_ circle');
+	// tlErosion =  new TimelineMax({ paused: true });
+	var controller = new ScrollMagic.Controller();
+	tlErosion =  new TimelineMax({ paused: false })
+	tlErosion.set([$erosionBlue], {autoAlpha: 0, yPercent: '-100'});
+
+	// tlErosion
+	// 	.set([$erosionBlue], {autoAlpha: 0, yPercent: '-100'})
+	// 	.to([$erosionRock1], 0.5, {autoAlpha: 1})
+	// 	.to([$erosionRock2], 0.5, {autoAlpha: 1})
+	// 	.to([$erosionRock3], 0.5, {autoAlpha: 1})
+	// 	.to([$erosionRock4], 0.5, {autoAlpha: 1})
+	// 	.to([$erosionRock5], 0.5, {autoAlpha: 1})
+	// 	.to([$erosionBlue], 0.5, {autoAlpha: 1, yPercent: '0'});
 	
+	// var ctrlErosion = new ScrollMagic.Controller({
+	// 	globalSceneOptions: {
+	// 		triggerHook: 'onEnter',
+	// 		triggerElement: ".erosion-scene"
+	// 	}
+	// });
 
-	var controllerMapAnim = new ScrollMagic.Controller();
-	TweenMax.defaultOverwrite = false;
-	TweenLite.defaultOverwrite = false;
+	// new ScrollMagic.Scene({
+	//   offset: 500
+	// })
+	// .addTo(ctrlErosion)
+	// .on("enter", function (e) {
+	// 	tlErosion.play();	
+	// });
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionRock1, 0.5, {opacity: 1}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
 
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionRock2, 0.5, {opacity: 1, delay: 0.5}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionRock3, 0.5, {opacity: 1, delay: 1}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionRock4, 0.5, {opacity: 1, delay: 1.5}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionRock5, 0.5, {opacity: 1, delay: 2}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+	new ScrollMagic.Scene({
+			triggerElement: ".erosion-scene"
+		})
+		.setTween($erosionBlue, 0.5, {autoAlpha: 1, yPercent: '0', delay: 2.5}) // trigger a TweenMax.to tween
+		//.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+// ---------------------------------------------------------------------------------------------------------------------//
+// ---------------------------------------------------------------------------------------------------------------------//
+// ---------------------------------------------------------------------------------------------------------------------//
 	
+	var ctrlTassement = new ScrollMagic.Controller();
+	var $tassementCourbe = $('section.tassement-scene svg#tassement-svg #courbe path');
 
 	new ScrollMagic.Scene({
-			triggerElement: 'h4', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setClassToggle(".fixed-map-wrapper svg #path-phoenix-nogales path", "is-drawn")
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h4', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setTween($dotAnim1, 0.5, {scale: 1.3, transformOrigin:'50% 50%', delay: 1.3,  ease: Bounce.easeOut}) 
-		//.setClassToggle(".fixed-map-wrapper svg #nogales-dot #step1-dot_2_ circle", "is-blue")
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-
-	var $segmentAnim2 = $('.fixed-map-wrapper svg #path-nogales-tombstone-elpaso path');
-	var $dotAnim2 = $('.fixed-map-wrapper svg #tombstone-dot #step1-dot_3_ circle');
-	var $dotAnim2b = $('.fixed-map-wrapper svg #elpaso-dot #step1-dot circle');
-
-	// $segmentAnim2.hide();
-	// $dotAnim2.hide();
-	// $dotAnim2b.hide();
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step2', 
+			triggerElement: 'section.tassement-scene', 
 			triggerHook: 0.5
 		})
 		//.setTween(".fixed-map-wrapper svg #path-phoenix-nogales path", {autoAlpha: 0, yPercent: '30%', ease: Power0.easeInOut})
-		.setClassToggle(".fixed-map-wrapper svg #path-phoenix-nogales path", "st12") 
+		.setClassToggle("section.tassement-scene svg#tassement-svg #courbe path", "is-drawn") 
 		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
+		//.addIndicators() // add indicators (requires plugin)
+		.addTo(ctrlTassement);
 
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step2', 
-			triggerHook: 0.5
-		})
-		.setClassToggle(".fixed-map-wrapper svg #path-nogales-tombstone-elpaso path", "is-drawn2")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step2', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setTween($dotAnim2b, 0.5, {scale: 1.3, transformOrigin:'50% 50%', delay: 1.3,  ease: Bounce.easeOut}) 
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
+	//$tassementCourbe.hide();
 
 // ---------------------------------------------------------------------------------------------------------------------//
 // ---------------------------------------------------------------------------------------------------------------------//
 // ---------------------------------------------------------------------------------------------------------------------//
 
-	var $segmentAnim3 = $('.fixed-map-wrapper svg #path-elpaso-tucson-sells path');
-	var $dotAnim3 = $('.fixed-map-wrapper svg #tucson-dot #step1-dot_4_ circle');
-	var $dotAnim3b = $('.fixed-map-wrapper svg #sells-dot #step1-dot_5_ circle');
+	var ctrlCrack = new ScrollMagic.Controller();
+	var $crack2 = $('.crack2');
 
 	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step3', 
+			triggerElement: 'section.crack-scene', 
 			triggerHook: 0.5
 		})
-		//.setTween(".fixed-map-wrapper svg #path-phoenix-nogales path", {autoAlpha: 0, yPercent: '30%', ease: Power0.easeInOut})
-		.setClassToggle(".fixed-map-wrapper svg #path-nogales-tombstone-elpaso path", "st12") 
+		.setClassToggle(".crack2", "is-visible") 
 		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step3', 
-			triggerHook: 0.5
-		})
-		.setClassToggle(".fixed-map-wrapper svg #path-elpaso-tucson-sells path", "is-drawn3")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step3', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setTween($dotAnim3b, 0.5, {scale: 1.3, transformOrigin:'50% 50%', delay: 1.3,  ease: Bounce.easeOut}) 
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-
-	var $segmentAnim4 = $('.fixed-map-wrapper svg #path-sells-yuma path');
-	var $dotAnim4 = $('.fixed-map-wrapper svg #ajo-dot #step1-dot_1_ circle');
-	var $dotAnim4b = $('.fixed-map-wrapper svg #yuma-dot #step1-dot_7_ ellipse');
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step4', 
-			triggerHook: 0.5
-		})
-		//.setTween(".fixed-map-wrapper svg #path-phoenix-nogales path", {autoAlpha: 0, yPercent: '30%', ease: Power0.easeInOut})
-		.setClassToggle(".fixed-map-wrapper svg #path-elpaso-tucson-sells path", "st12") 
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step4', 
-			triggerHook: 0.5
-		})
-		.setClassToggle(".fixed-map-wrapper svg #path-sells-yuma path", "is-drawn4")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step4', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setTween($dotAnim4b, 0.5, {scale: 1.3, transformOrigin:'50% 50%', delay: 1.3,  ease: Bounce.easeOut}) 
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------//
-
-	var $segmentAnim5 = $('.fixed-map-wrapper svg #path-yuma-sandiego path');
-	var $dotAnim5 = $('.fixed-map-wrapper svg #sandiego-dot #step1-dot_8_ circle');
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step5', 
-			triggerHook: 0.5
-		})
-		//.setTween(".fixed-map-wrapper svg #path-phoenix-nogales path", {autoAlpha: 0, yPercent: '30%', ease: Power0.easeInOut})
-		.setClassToggle(".fixed-map-wrapper svg #path-sells-yuma path", "st12") 
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step5', 
-			triggerHook: 0.5
-		})
-		.setClassToggle(".fixed-map-wrapper svg #path-yuma-sandiego path", "is-drawn5")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
-
-	new ScrollMagic.Scene({
-			triggerElement: 'h3#anim-step5', 
-			triggerHook: 0.5
-			//duration: (stepCitern * 10)
-		})
-		//.setPin('section.citern', {pushFollowers: true})
-		.setTween($dotAnim5, 0.5, {scale: 1.3, transformOrigin:'50% 50%', delay: 1.3,  ease: Bounce.easeOut}) 
-		//.setTween(tween)
-		//.setClassToggle(".fixed-map-wrapper svg #sandiego-dot path", "is-hidden")
-		.offset( 0 ) //tip top
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controllerMapAnim);
+		//.addIndicators() // add indicators (requires plugin)
+		.addTo(ctrlCrack);
 
 });
